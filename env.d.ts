639537a00cf94614f2a9f898b0d5895d1aa19c1b
Aleksey Svetlitskiy <a.svetlitskiy@gmail.com>
2025-02-25 12:38:39 -0300

    feat(deploy) configure pipeline
    - add deploy to GitHub pages
    - add check build on PR

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+interface ImportMetaEnv {
+  readonly VITE_APP_HOME_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: process.env.VITE_APP_HOME_URL || '/',
   plugins: [react()],
 });
